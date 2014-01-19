29729c57b71f1f29d12135e00461b3495efa1b4b
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2014-01-19 10:09:20 +0100

    Switched to clj-http from URLConnections
    This takes Clojure maps for requests natively, so I don't need to do the
    translation here.
    To incorporate session data and similar in a request map, you now use
    (partial merge-with conj).

--- project.clj
+++ project.clj
@@ -5,9 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
-                 [org.clojure/data.json "0.2.3"]])
+                 [org.clojure/data.json "0.2.3"]
+                 [clj-http "0.7.8"]])
 
 
 
 
-
+
+
--- src/sati_bot/reddit.clj
+++ src/sati_bot/reddit.clj
@@ -1,61 +1,58 @@
 (ns sati-bot.reddit
-  (:require [sati-bot.core :as core]
-            [clojure.data.json :as json]
-            [clojure.java.io :as io])
+  (:require [clojure.data.json :as json]
+            [clj-http.client :as client])
   (:import [java.net URL HttpURLConnection URLEncoder])
   )
 
 (def reddit-api "http://www.reddit.com/api/")
+(def url-encoded "application/x-www-form-urlencoded")
 
 (defn- post-content
-  "Takes a map, returns an HTTP POST content filling in a form with the given keys and values"
+  "Takes a map, returns an HTTP POST body filling in a form with the given keys and values"
   [input]
-  (let[pairs (map (fn [[name value]] (str name "=" (URLEncoder/encode (str value) "UTF-8"))) input)]
+  (let[pairs (map (fn [[name value]] (str name "=" (URLEncoder/encode (if (nil? value) "" (str value)) "UTF-8") )) input)]
     (apply str (butlast (interleave pairs (repeat "&"))))
     ))
 
 
-(defn request "Prepare a request to reddit, based on a request map.
-  This is a pure function and does no IO - call .connect on the returned URLConnection object to do that."
-  [{:keys [URL method data cookie modhash]}]
-  (let[URL (if (instance? java.net.URL URL) URL (URL. (str reddit-api URL))) ;Coerce to java.net.URL if not already an instance.
-       conn (.openConnection URL)
-       ]
-    (.setRequestMethod conn method)
-    (.setRequestProperty conn "User-Agent" "sati_bot/0.1.0 by lobsang_ludd")
-    (when cookie (.setRequestProperty conn "Cookie" cookie))
-    (when modhash (.setRequestProperty conn "X-Modhash" modhash))
-    (when (and (= "POST" method) data)
-      (.setDoOutput conn true)
-      (spit (.getOutputStream conn) (post-content data))
-      )
-    conn))
+(defn request "Send a request to reddit."
+  ([requestmap]
+   (client/request
+    (merge-with conj requestmap {:headers {"User-Agent" "sati_bot/0.1.0 by lobsang_ludd"}})
+    )
+   )
+  ([requestmap session-data]
+   (request (merge-with conj requestmap session-data)))
+  )
 
 (defn login
   "Make a request map for logging in as the user with given credentials.
   This call returns cookies and modhashes identifying the user account and session, which some calls require."
   [{:keys [username password] :as credentials}]
-  {:URL "login"
-   :method "POST"
-   :data {"api_type" "json"
-          "user" username
-          "passwd" password}}
+  {:url (str reddit-api "login")
+   :method :post
+   :content-type url-encoded
+   :body (post-content {"api_type" "json"
+                        "user" username
+                        "passwd" password})}
   )
 
+
+
 (def me "A request map for the 'me' call."
-  {:method "GET"
-   :URL "me.json"
+  {:method :get
+   :url (str reddit-api "me.json")
    })
 
 (defn get-session-data
-  "Get a map of the user and session data out of a login request.
-  These must be included with API calls that 'write' to reddit. Will trigger sending/connecting if not already done."
-  [request]
-  (let[cookie (str (get (.getHeaderFields request) "Set-Cookie"))
-       content (-> (.getInputStream request) io/reader (json/read :key-fn keyword))
-       modhash (get-in content [:json :data :modhash])
+  "Get a map of the user and session data out of a login response.
+  These must be included with API calls that 'write' to reddit."
+  [{:keys [cookies body] :as response}]
+  (let[json (json/read-str (:body response) :key-fn keyword)
+       errors (get-in json [:json :errors])
+       modhash (get-in json [:json :data :modhash])
        ]
-    {:cookie cookie :modhash modhash}))
+    {:cookies cookies :headers {"X-Modhash" modhash}}))
 
 (defn do-login
   "Convenience function: Logs in with the given credentials, reads the returned message and makes a session map.
@@ -75,17 +72,19 @@
   * :text is optional and if provided will be the (Markdown formatted) body of the text post.
   * :resubmit true asks reddit to resubmit the link in the case that it's already posted to the subreddit."
   [title subreddit & options]
-  (let[{:keys [kind link URL text resubmit] } (apply hash-map options)
-       kind (if (string? kind) kind (if link "link" "text"))
+  (let[{:keys [kind link URL text resubmit] } options
+       kind (if (string? kind) kind (if link "link" "self"))
        resubmit (if resubmit true false)]
-    {:URL (str reddit-api "submit")
-     :method "POST"
-     :data {"title" title
-            "sr" subreddit
-            "kind" kind
-            "text" text
-            "url" URL
-            "resubmit" resubmit
-            "then" "comments"}
+    {:url (str reddit-api "submit")
+     :method :post
+     :content-type url-encoded
+     :body (post-content {"title" title
+                            "sr" subreddit
+                            "kind" kind
+                            "text" (if text text "")
+                            "url" URL
+                            "resubmit" resubmit
+                            "then" "comments"
+                            "api-type" "json"})
      }))
-
+
